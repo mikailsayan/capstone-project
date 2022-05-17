3cc723fb34f9d7121792aca3ebdd425e7fedd261
Mikail Sayan <msayan@outlook.de>
2022-05-17 15:02:58 +0200

    Current status

--- src/components/styled-components/StyledList.js
+++ src/components/styled-components/StyledList.js
@@ -18,16 +18,17 @@ const StyledList = styled.li`
 	${({ variant }) =>
 		variant === 'todolist-small' &&
 		`
-    width: 30.3rem;
-    height: 3.6rem;
-    background-color: var(--dark-grey);
-    border: none;
-    border-radius: 4rem;
-    display: flex;
-    align-items: center;
-    font-size: 1.6rem;
-    gap: 1.2rem;
-    padding: 0 1rem 0 1rem;
+        position: relative;
+        width: 30.3rem;
+        height: 3.6rem;
+        background-color: var(--dark-grey);
+        border: none;
+        border-radius: 4rem;
+        display: flex;
+        align-items: center;
+        font-size: 1.6rem;
+        gap: 1.2rem;
+        padding: 0 1rem 0 1rem;
 `}
 `;
 
