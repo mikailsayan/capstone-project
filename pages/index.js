db64760e2a9537e02fc3eed39f94d28c990b7ea6
Mikail Sayan <msayan@outlook.de>
2022-05-17 15:03:46 +0200

    Add Featureview to index

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,13 @@
 import AddProject1 from '../src/components/AddProject1';
 import ProjectCardGridHome from '../src/components/Projectcard Grid Home/ProjectCardGridHome';
 import FeatureCardSmall from '../src/components/Featurecard Small/FeatureCardSmall';
+import FeatureView from '../src/components/FeatureView/FeatureView';
 
 export default function Home() {
 	return (
 		<div>
 			<main>
+				<FeatureView />
 				<FeatureCardSmall />
 				<ProjectCardGridHome />
 				<AddProject1 />
