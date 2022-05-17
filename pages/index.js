9b22317c352d462b85923302e05412acdb10c3d9
Mikail Sayan <msayan@outlook.de>
2022-05-17 15:01:12 +0200

    commit changes

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,12 @@
 import AddProject1 from '../src/components/AddProject1';
+import FeatureView from '../src/components/FeatureView/FeatureView';
 import ProjectCardGridHome from '../src/components/Projectcard Grid Home/ProjectCardGridHome';
 
 export default function Home() {
 	return (
 		<div>
 			<main>
+				<FeatureView />
 				<ProjectCardGridHome />
 				<AddProject1 />
 			</main>
