fc9fda60d8caa521e1fbaaa32602bc8cdb402ffa
Mikail Sayan <msayan@outlook.de>
2022-05-20 12:43:02 +0200

    Add structure

--- pages/index.js
+++ pages/index.js
@@ -2,11 +2,13 @@ import AddProject1 from '../src/components/AddProject1';
 import FeatureViewCard from '../src/components/FeatureView/FeatureView';
 import ProjectCardListHome from '../src/components/ProjectCardListHome/ProjectCardListHome';
 import FeatureCardListProject from '../src/components/FeatureCardListProject/FeatureCardListProject';
+import EditFeatureProject from '../src/components/EditFeatureProject/EditFeatureProject';
 
 export default function Home() {
 	return (
 		<div>
 			<main>
+				<EditFeatureProject />
 				<FeatureViewCard />
 				<FeatureCardListProject />
 				<ProjectCardListHome />
