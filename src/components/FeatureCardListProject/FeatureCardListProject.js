3590b6210553fee1add88802f59a53554722cdef
Mikail Sayan <msayan@outlook.de>
2022-05-25 13:15:23 +0200

    Add arrow svg for dropdown

--- src/components/FeatureCardListProject/FeatureCardListProject.js
+++ src/components/FeatureCardListProject/FeatureCardListProject.js
@@ -5,6 +5,7 @@ import StyledDiv from '../styled-components/StyledDiv';
 import StyledParagraph from '../styled-components/StyledParagraph';
 import Typography from '../styled-components/Typography';
 import useStore from '../../hooks/useStore';
+import MySVG from '../MySVG';
 
 export default function FeatureCardListProject() {
 	const projectszustand = useStore(state => state.projectszustand);
@@ -24,6 +25,7 @@ export default function FeatureCardListProject() {
 					<Typography variant="h2" component="h2">
 						Projektname
 					</Typography>
+					<MySVG variant="dropdown" size="2.2rem" />
 					{dropdown[0].isClicked ? (
 						<StyledDiv variant="dropdown-menu">
 							{projectszustand.map(project => {
