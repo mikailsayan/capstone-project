7cd515b77f63cf93a260bcf904754cf097c204fb
Mikail Sayan <msayan@outlook.de>
2022-06-03 12:13:57 +0200

    skinning code

--- pages/projects/index.js
+++ pages/projects/index.js
@@ -3,17 +3,9 @@ import EditTodo from '../../src/components/EditTodo/EditTodo';
 import StyledMain from '../../src/components/styled-components/StyledMain';
 import StyledArticle from '../../src/components/styled-components/StyledArticle';
 import Typography from '../../src/components/styled-components/Typography';
-import StyledDiv from '../../src/components/styled-components/StyledDiv';
-import StyledParagraph from '../../src/components/styled-components/StyledParagraph';
-import MySVG from '../../src/components/MySVG';
-import useStore from '../../src/hooks/useStore';
-import { useState } from 'react';
+import Dropdown from '../../src/components/Dropdown';
 
 export default function Home() {
-	const [dropdownIsOpen, setDropdownIsOpen] = useState(false);
-	const projects = useStore(state => state.projects);
-	const selectProject = useStore(state => state.selectProject);
-
 	return (
 		<div>
 			<StyledMain>
@@ -21,39 +13,7 @@ export default function Home() {
 					<Typography variant="h1" size="2rem">
 						Projekte
 					</Typography>
-					<StyledDiv
-						variant="dropdown"
-						onClick={() => {
-							setDropdownIsOpen(true);
-						}}
-					>
-						<MySVG variant="dropdown" size="2.3rem" />
-						{dropdownIsOpen ? (
-							<StyledDiv variant="dropdown-menu">
-								{projects.map(project => {
-									const index = projects.findIndex(
-										projectIndex => projectIndex.id === project.id
-									);
-									return (
-										<StyledDiv
-											key={project.id}
-											variant="dropdown-content"
-											onClick={() => {
-												selectProject(index);
-												setDropdownIsOpen(false);
-											}}
-										>
-											<StyledParagraph variant="dropdown-content">
-												{project.name}
-											</StyledParagraph>
-										</StyledDiv>
-									);
-								})}
-							</StyledDiv>
-						) : (
-							''
-						)}
-					</StyledDiv>
+					<Dropdown />
 				</StyledArticle>
 				<FeatureCardList />
 				<EditTodo />
---
+++ src/components/Dropdown.js
@@ -0,0 +1,47 @@
+import StyledDiv from '../../src/components/styled-components/StyledDiv';
+import StyledParagraph from '../../src/components/styled-components/StyledParagraph';
+import MySVG from '../../src/components/MySVG';
+import useStore from '../../src/hooks/useStore';
+import { useState } from 'react';
+
+export default function Dropdown() {
+	const [dropdownIsOpen, setDropdownIsOpen] = useState(false);
+	const projects = useStore(state => state.projects);
+	const selectProject = useStore(state => state.selectProject);
+
+	return (
+		<StyledDiv
+			variant="dropdown"
+			onClick={() => {
+				setDropdownIsOpen(true);
+			}}
+		>
+			<MySVG variant="dropdown" size="2.3rem" />
+			{dropdownIsOpen ? (
+				<StyledDiv variant="dropdown-menu">
+					{projects.map(project => {
+						const index = projects.findIndex(
+							projectIndex => projectIndex.id === project.id
+						);
+						return (
+							<StyledDiv
+								key={project.id}
+								variant="dropdown-content"
+								onClick={() => {
+									selectProject(index);
+									setDropdownIsOpen(false);
+								}}
+							>
+								<StyledParagraph variant="dropdown-content">
+									{project.name}
+								</StyledParagraph>
+							</StyledDiv>
+						);
+					})}
+				</StyledDiv>
+			) : (
+				''
+			)}
+		</StyledDiv>
+	);
+}
